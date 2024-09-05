44af29e3b88a2a8e21d24bf2a3e85a5807e0c97a
catwarlock87 <catwarlock@yandex.ru>
2024-09-05 13:21:22 +0300

    Добавили кнопку для загрузки нового изображения

--- Cataas.py
+++ Cataas.py
@@ -2,11 +2,12 @@ from tkinter import *
 from PIL import Image, ImageTk
 import requests
 from io import BytesIO
+# import certifi
 
 
-def load_image():
+def load_image(url):
     try:
-        response = requests.get(url)
+        response = requests.get(url, timeout=5) # timeout=5 - увеличили время ожидания
         response.raise_for_status()
         image_data = BytesIO(response.content)
         img = Image.open(image_data)
@@ -16,6 +17,14 @@ def load_image():
         return None
 
 
+def set_image():
+    img = load_image(url)
+
+    if img:
+        label.config(image=img)
+        label.image = img
+
+
 window = Tk()
 window.title("Cats!")
 window.geometry('600x480')
@@ -23,11 +32,11 @@ window.geometry('600x480')
 label = Label()
 label.pack()
 
-url = "https://cataas.com/cat"
-img = load_image(url)
+update_button = Button(text="Обновить", command=set_image)
+update_button.pack()
 
-if img:
-    label.config(image=img)
-    label.image = img
+url = "https://cataas.com/cat"
+
+set_image()
 
 window.mainloop()
